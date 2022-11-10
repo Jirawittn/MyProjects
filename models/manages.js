6a9f3a266451ca5ac14498d6e1bdfd3f8fff7721
jirawittn <jirawitth@kkumail.com>
2022-11-10 22:34:34 +0700

    Master

---
+++ models/manages.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const mongo = require('mongodb')
+const dbUrl='mongodb://localhost:27017/myProject'
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser:true
+})
+
+const db = mongoose.connection
+const Schema = mongoose.Schema
+
+const manageSchema = new Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    price:{
+        type:Number,
+        require:true
+    },
+    number:{
+        type:Number,
+        require:true
+    },
+    dangerNumber:{
+        type:Number,
+        require:true
+    },
+    safeNumber:{
+        type:Number,
+        require:true
+    },
+}, { timestamps:true }
+)
+
+const Manages = module.exports = mongoose.model("storages", manageSchema)
+module.exports.createProduct = function(newProducts, callback){
+    newProducts.save(callback)
+}
+
+module.exports.getAllProducts = function(data){
+    Manages.find(data)
+}
+
+module.exports.deleteProduct = function(id, callback){
+    Manages.findByIdAndDelete(id, callback)
+}
+
+module.exports.saveProduct = function(model, data){
+    model.save(data)
+}
+
+
